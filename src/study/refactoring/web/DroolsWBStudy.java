6215640ae398883ab9e047e4c37af120e601f955
Alexil Ferreira <alexil.ferreira@gmail.com>
2020-03-05 23:48:05 -0300

    Correction of the name of the results file for the Drools study project.
    Signed-off-by: Alexil Ferreira <alexil.ferreira@gmail.com>

--- src/study/refactoring/web/DroolsWBStudy.java
+++ src/study/refactoring/web/DroolsWBStudy.java
@@ -18,7 +18,7 @@ public class DroolsWBStudy {
 		String urlRepository = "https://github.com/kiegroup/drools-wb.git";
 		String initialCommit = "8dd1e3c5de4b4676e10e1e42a666b753c4df2119"; // 6.0.0 - 2013.11.19
 		String finalCommit = "564c064a7dede2ad9ea9422feffee2d731f44daf"; // 7.31.0.Final - 2019.12.09
-		String resultFile= System.getProperty("user.dir") + "\\refactoring\\aet";
+		String resultFile= System.getProperty("user.dir") + "\\refactoring\\drools-wb";
 
 		List<LimiarTecnica> listThresholdsTechiniques = CarregaSalvaArquivo
 				.carregarLimiares(System.getProperty("user.dir") + "\\thresholds\\web2020\\");
